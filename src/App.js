a6747dc29b5daf7244d995f20c5331f8c7cc4805
Daniel Evers <contact@danielevers.online>
2020-01-20 14:11:25 -0700

    react 1-mini done

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import { render } from '@testing-library/react';
+
+class App extends Component {
+  constructor(){ 
+    super();
+    this.state = {
+      friends: [],
+      picture: '',
+      name: ''
+    };
+  }
+
+  updatePicture = (value) => {
+    this.setState({
+      picture :value
+    });
+  }
+  
+updateName = (value) => {
+  this.setState({
+    name: value
+  });
+}
+
+addFriend() {
+  const { friends, picture, name } = this.state;
+
+  let newFriends = friends.slice();
+  newFriends.push({ picture, name });
+
+  this.setState({ friends: newFriends, picture: '', name: '' });
+}
+
+render() {
+  const friends = this.state.friends.map((friend,index) => ( <div key={`friend-${index}-${friend.name}`}>
+  <img width="100px" src={friend.picture} />
+    <span>{friend.name}</span>
+    
+  </div>
+  ));
+  return (
+    <div><span>Picture:</span>
+      <input onChange={(event) => this.updatePicture(event.target.value)} value={this.state.picture}/>
+      <span>Name:</span>
+      <input onChange={(event) =>this.updateName(event.target.value)} value={this.state.name}/>
+      <button onClick={() => this.addFriend()}>Add Friend</button> {friends}
+    </div>
+    );
+  }
+}
+
+export default App;
